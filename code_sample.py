8b1ed9deccc672c85ad03a513568df4e57bffc04
Robyn Lundin <robyn@hartung.com>
2018-04-17 11:15:43 -0700

    Add detail to docstring in code_sample.py.

--- code_sample.py
+++ code_sample.py
@@ -1,5 +1,7 @@
 def printing_loop():
-    """A simple loop that prints 'I'm a loop' 5 times."""
+    """A simple loop that prints 'I'm a loop' 5 times.
+
+    Takes no arguments or parameters."""
 
     for i in range(5):
         print "I'm a loop."
